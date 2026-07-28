a11c0c2acbe62b5c408806a21dbb4019bddd3831
Madhu-661 <madhukodari82@gmail.com>
2026-07-28 16:25:38 +0530

    Add files via upload

---
+++ crud.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+from models import Student
+
+
+def create_student(db: Session, student):
+    new_student = Student(
+        name=student.name,
+        age=student.age,
+        course=student.course
+    )
+
+    db.add(new_student)
+    db.commit()
+    db.refresh(new_student)
+
+    return new_student
+def get_students(db: Session):
+    return db.query(Student).all()
